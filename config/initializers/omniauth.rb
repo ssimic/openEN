cde07fc0b917ea4d2314c8b29dd239f5c916910f
Momotaro Tsuyuzaki <momotaro@plus-n.biz>
2012-12-25 09:46:23 +0900

    Add Omniauth-Tumblr to Gemfile.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+  # provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
+   provider :tumblr, ENV['TUMBLR_KEY'], ENV['TUMBLER_SECRET']
 end
